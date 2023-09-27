7671f035afa59689245c5caed0ffbfc0ce5d7508
Fenya2 <fenya74.09@gmail.com>
2023-09-27 21:11:51 +0500

    test send messages using telegram bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,21 @@
+import config.BotConfig;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) throws TelegramApiException{
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new TelegramBot());
-        System.out.println("test");
+        BotConfig bc;
+        try {
+            bc = new BotConfig("src/main/resources/telegram_bot.conf");
+        } catch (FileNotFoundException e) {
+            System.err.println("config file not found");
+            return;
+        }
+        TelegramBot bot = new TelegramBot(bc);
+        botsApi.registerBot(bot);
     }
 }
--- src/main/java/TelegramBot.java
+++ src/main/java/TelegramBot.java
@@ -1,19 +1,41 @@
+import lombok.NonNull;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import config.BotConfig;
 
 public class TelegramBot extends TelegramLongPollingBot {
+    BotConfig botConfig;
+    TelegramBot(@NonNull BotConfig botConfig) {
+        super(botConfig.getToken());
+        this.botConfig = botConfig;
+    }
     @Override
     public String getBotUsername() {
-        return "Test";
+        return botConfig.getName();
     }
-
-    @Override
-    public String getBotToken() {
-        return "YOUR TOKEN HERE";
-    }
-
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("test");
+        if(update.hasMessage()) {
+            Message msg = update.getMessage();
+            User user = msg.getFrom();
+            switch (msg.getText()) {
+                case "/greetings" -> sendMessage(user.getId(), "greetings, " + user.getUserName() + "!");
+                case "/help" -> sendMessage(user.getId(), "help message");
+            }
+        }
+    }
+    private void sendMessage(Long userID, String text) throws RuntimeException {
+        SendMessage sm = SendMessage.builder()
+                .chatId(userID.toString())
+                .text(text).build();
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
