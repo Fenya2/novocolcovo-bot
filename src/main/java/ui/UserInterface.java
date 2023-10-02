d962ed80bf9fa1c3a59231302d62ec2798b36ff0
adfips <arturkhalilov@mail.ru>
2023-10-02 22:05:46 +0500

    do task one

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -4,5 +4,5 @@ package ui;
     Интерфейс для непосредственного взаимодействия с пользователем. Вот его надо будет реализовывать для любой платформы
  */
 public interface UserInterface {
-    public void send_text_message(int recipient, String message);
+    public void sendTextMessage(Long recipient, String message);
 }
