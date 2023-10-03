c1784569c860652f3472d17fa164f9dd795f0eac
Fenya2 <fenya74.09@gmail.com>
2023-10-03 15:31:07 +0500

    tests added

--- src/test/java/ui/TelegramBotTest.java
+++ src/test/java/ui/TelegramBotTest.java
@@ -1,15 +1,25 @@
+package ui;
+
 import config.TelegramBotConfig;
+import org.junit.Test;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import ui.TelegramBot;
-import ui.UserInterface;
 
-public class Main {
-    public static void main(String[] args) throws TelegramApiException {
+public class TelegramBotTest {
+    @Test
+    public void registerBotTest() throws TelegramApiException {
         UserInterface bot = new TelegramBot(new TelegramBotConfig("src/main/resources/config/TelegramBotConfig.json"));
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot((LongPollingBot) bot);
+    }
+
+    @Test
+    public void sendMessageTest() throws TelegramApiException {
+        UserInterface bot = new TelegramBot(new TelegramBotConfig("src/main/resources/config/TelegramBotConfig.json"));
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot((LongPollingBot) bot);
+        bot.sendTextMessage(517043435L, "It's test message");
     }
 }
