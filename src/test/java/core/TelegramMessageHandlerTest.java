3e5953e455ed96cdb1cbac3219cdf491a4e2a858
Fenya2 <fenya74.09@gmail.com>
2023-10-08 21:05:55 +0500

    readme added, telegramMessageHandlerTest a little updated

--- src/test/java/core/TelegramMessageHandlerTest.java
+++ src/test/java/core/TelegramMessageHandlerTest.java
@@ -12,16 +12,17 @@ public class TelegramMessageHandlerTest {
         TelegramMessageHandler tmh = new TelegramMessageHandler();
         User user = new User();
         user.setId(0L);
+        user.setUserName("JohnDoe");
         Message msg = new Message();
         msg.setText("/start");
         msg.setFrom(user);
         SendMessage sm = tmh.parse(msg);
-        Assert.assertEquals(sm.getText(), "Hello!");
-        msg.setText("/greetings");
-        sm = tmh.parse(msg);
-        Assert.assertEquals(sm.getText(), "Hello!");
+        Assert.assertEquals("Привет, JohnDoe! Я эхо бот. Отправь /help, чтобы узнать, что я умею", sm.getText());
         msg.setText("/help");
         sm = tmh.parse(msg);
-        Assert.assertEquals(sm.getText(), "It is a help message.");
+        Assert.assertEquals("Я эхо бот. Отправь мне любой текст и я отправлю тебе его же в ответ.", sm.getText());
+        msg.setText("some text need to be echoed");
+        sm = tmh.parse(msg);
+        Assert.assertEquals("some text need to be echoed", sm.getText());
     }
 }
