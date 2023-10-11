ffc41b7b14d5fbf3522a2f6032b0c33af0683b5d
adfips <arturkhalilov@mail.ru>
2023-10-11 16:39:21 +0500

    add the task2 skeleton

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class User {
+    private int id;
+    private String platform;
+    private String idOnPlatform;
+
+    private String login;
+    private String name;
+    private String phone;
+}
